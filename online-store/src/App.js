340e1bfeb744244942ec7301c9680df32a61382a
hiramr97 <hiram.rodriguez97@icloud.com>
2022-12-08 12:10:33 -0500

    added file structure

---
+++ online-store/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Create from "./Components/Create";
+import Description from "./Components/Description";
+import Edit from "./Components/Edit";
+import Header from "./Components/Header";
+import Homepage from "./Components/Homepage";
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <p>Hello</p>
+    </div>
+  );
+}
+
+export default App;
