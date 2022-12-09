379028467d184661351183fce8926dcd297c6730
hiramr97 <hiram.rodriguez97@icloud.com>
2022-12-09 09:59:27 -0500

    added state for items

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import Description from "./Components/Description";
 import Edit from "./Components/Edit";
 import Header from "./Components/Header";
 import Homepage from "./Components/Homepage";
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
+  const [items, setAllItems] = useState({})
+
   return (
     <div>
     <Header/>
     <Routes>
-      <Route path="/" element={<Homepage/>}/>
+      <Route path="/" element={<Homepage items={items} setAllItems={setAllItems}/>}/>
       <Route path="/description/:id" element={<Description/>}/>
       <Route path="/edit/:id" element={<Edit/>}/>
       <Route path="/create" element={<Create/>}/>
--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,7 +1,12 @@
 import React from "react";
+import axios from "axios";
 
-const Homepage = () => {
-    
+const Homepage = (props) => {
+    const getItems = () => {
+        axios.get('localhost:8000/api/online-store/items').then(response => {
+            console.log(response.data)
+        })
+    }
 };
 
 export default Homepage;
