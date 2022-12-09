69aa8fb634446fa02618ca6e7b581dfab0e759f0
hiramr97 <hiram.rodriguez97@icloud.com>
2022-12-09 10:51:59 -0500

    added links to images

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -3,30 +3,31 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 const Homepage = (props) => {
-    const getItems = () => {
-        axios.get('http://localhost:8000/api/online-store/items').then(response => {
-            props.setItems(response.data)
-        })
-    }
-    useEffect(() => {
-        getItems()
-    }, [])
+  const getItems = () => {
+    axios
+      .get("http://localhost:8000/api/online-store/items")
+      .then((response) => {
+        props.setItems(response.data);
+      });
+  };
+  useEffect(() => {
+    getItems();
+  }, []);
 
-    if (props.items === undefined) return;
+  if (props.items === undefined) return;
 
-    const newData = props.items.map((items, key) => {
-        return(
-            <div key={key}>
-                <img src={items.images} alt={items.name}/>
-                <p>{items.price}</p>
-            </div>
-        )
-    })
-    return(
-        <div>
-            {newData}
-        </div>
-    )
+  const newData = props.items.map((items, key) => {
+    return (
+      <div key={key}>
+        <Link to={`description/${items.id}`}>
+        <img src={items.images} alt={items.name} />
+        </Link>
+        <p className="Price">{items.price}</p>
+      </div>
+    );
+  });
+
+  return <div>{newData}</div>;
 };
 
 export default Homepage;
