46996551ad48e5f29c01409dc05744f5962f99cd
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-09 14:22:43 -0600

    Set up form and button for Create component

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState} from "react";
+import axios from 'axios'
 
 const Create = () => {
+    
+
+    return(
+        <form>
+            <input placeholder="Item name" name="title"></input>
+            <input placeholder="Description" name="description"></input>
+            <input placeholder="Image" name="images"></input>
+            <input placeholder="Price" name="price"></input>
+            <button>Submit</button>
+        </form>
+    )
     
 };
 
