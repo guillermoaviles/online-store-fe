3f33d6d8d8576e98cb5fcfb933ca9986e03eab6a
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-10 11:21:02 -0600

    Created EditButton Component

--- src/Components/Edit.js
+++ src/Components/Edit.js
@@ -1,16 +1,44 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { useParams } from 'react-router-dom'
 
 const Edit = () => {
+
+    const {id} = useParams();
     const [updateItem, setUpdateItem] = useState({
         title: "",
         description: "",
         images: "",
         price: ""
     })
-    return 
 
-    
-    
+    const updateItemCall = async () => {
+        try {
+          const change = await axios.put(`https://floral-bird-829.fly.dev/api/recipes/${id}`, updateItem)
+        }
+        catch (err) {
+          console.log(err)
+        }
+      }
+
+    const handleUpdateInput = (e) => {
+        const itemUpdateInput = {...updateItem};
+        itemUpdateInput[e.target.name] = e.target.value;
+        setUpdateItem(itemUpdateInput);
+    }
+
+    return (
+        <div>
+            <form>
+                <input placeholder="Item name" name="title" value={updateItem.title} onChange={handleUpdateInput}></input>
+                <input placeholder="Description" name="description" value={updateItem.description} onChange={handleUpdateInput}></input>
+                <input placeholder="Image" name="images" value={updateItem.images} onChange={handleUpdateInput}></input>
+                <input placeholder="Price" name="price" value={updateItem.price} onChange={handleUpdateInput}></input>
+                <button onClick={updateItemCall}>Submit</button>
+            </form>
+        </div>
+    )
+
 };
 
 export default Edit;
---
+++ src/Components/EditItemButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditItemButton = () => {
+  return (
+    <div>EditItemButton</div>
+  )
+}
+
+export default EditItemButton
