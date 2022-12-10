77116993827ba384808ec92a40575400a9fdea81
username <gjaviles7@gmail.com>
2022-12-10 14:32:57 -0500

    added sell button

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,11 +4,14 @@ import {Link} from "react-router-dom";
 
 const Header = () => {
     return (
-        <nav>
+        <nav className='nav'>
           <Link to='/' >
             <img src={icon}/>
           </Link>
-          <Link to='/' >
+          <Link to='/create' className='sell-button'>
+            <h2>Sell</h2>
+          </Link>
+          <Link to='/' className='cart'>
             <h2>My Cart</h2>
           </Link>
         </nav>
